c7b2f5cb9ce89971f518d849ec70db6292200591
Jeju <pipiru100@gmail.com>
2018-09-22 08:00:40 +0900

    dz

--- 04/app.js
+++ 04/app.js
@@ -1,3 +1,4 @@
+/* 강사님 코드
 var wrap = document.querySelector('.wrap');
 var str = '';
 
@@ -37,3 +38,17 @@ blocks.forEach(function(block){
 //   });
 
 // }
+*/
+
+var wrap=document.querySelector('.wrap');
+
+var str='';
+for(let i = 1; i <= 4; i++) {
+  for(let j = 1; j <= 4; j++) {
+      // let block = document.createElement('div');
+      // block.className = (i + j) % 2 == 1 ? 'black' : 'white';
+      // wrap.appendChild(block);
+      str += `<div class='${(i + j) % 2 == 1 ? 'b' : 'w'}'></div>`  
+  }
+}
+wrap.innerHTML=str;
