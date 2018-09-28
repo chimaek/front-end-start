93edeeb0399d4c7c293fe102996615ed2b412fc8
Jeju <pipiru100@gmail.com>
2018-09-28 13:07:18 +0900

    최종본

--- 04/app.js
+++ 04/app.js
@@ -1,3 +1,42 @@
+var wrap= document.querySelector('.wrap'); //wrap 클래스를 선택함
+var blockName=''; //클래스 이름정하는 변수
+var str=''; 
+var color=false; // true false 를 이용하여 색깔변경
+
+for(var i=0;i<4;i++){  //반복문을 돌려 4x4 체스판을 만듬
+  for(var j=0;j<4;j++){
+
+    if(color){
+      blockName='w';
+    }else{
+      blockName='b';
+    }
+    str += '<div class="'+ blockName +'"></div>';
+
+    color=!color;
+  }
+  color=!color;
+}
+wrap.innerHTML=str; 
+
+var allBoard=document.querySelectorAll('.wrap > div'); //wrap 클래스 밑에있는 div태그들을 전부 선택함.
+var changeBlock=null;
+
+for(var i=0;i<allBoard.length;i++){
+  allBoard[i].addEventListener('click',function select(event){
+
+    var board=event.currentTarget; //클릭한 보드를 찾음 current Target을 이용하여.
+    
+    if(changeBlock){
+      changeBlock.classList.remove("r")
+    }
+    
+    board.classList.add("r")
+    console.log(board)
+    changeBlock=board;
+  });
+}
+
 /* 강사님 코드
 var wrap = document.querySelector('.wrap');
 var str = '';
@@ -40,15 +79,3 @@ blocks.forEach(function(block){
 // }
 */
 
-var wrap=document.querySelector('.wrap');
-
-var str='';
-for(let i = 1; i <= 4; i++) {
-  for(let j = 1; j <= 4; j++) {
-      // let block = document.createElement('div');
-      // block.className = (i + j) % 2 == 1 ? 'black' : 'white';
-      // wrap.appendChild(block);
-      str += `<div class='${(i + j) % 2 == 1 ? 'b' : 'w'}'></div>`  
-  }
-}
-wrap.innerHTML=str;
