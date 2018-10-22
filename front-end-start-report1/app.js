d09ced9f3e525ba767e7f7877eef314d179342b5
Jeju <pipiru100@gmail.com>
2018-10-22 14:39:02 +0900

    중간고사

--- front-end-start-report1/app.js
+++ front-end-start-report1/app.js
@@ -3,12 +3,13 @@ var page;
 var allBoard=document.querySelectorAll('.nav > li'); //nav클래스 밑에 있는 리스트 선택
 var str='';
 var changeBlock=null;
-
+function closeLoding(){
+    document.getElementById('loading').style.display='none';
+}
+window.onload=closeLoding();
 //*********로딩화면 ************//
 document.getElementById('more').style.display="none"
-window.onload=function(){
-    document.getElementById('loading').style.display="none"
-}
+
 
 for(var i=0;i<allBoard.length;i++){
     //*************** 클릭하여 탭이 바뀌는 이벤트 및 리스트에 뿌려주기***************//
@@ -22,11 +23,16 @@ for(var i=0;i<allBoard.length;i++){
     changeBlock=board;
     str=''
     getJson();
+    document.getElementById('more').style.display="block";
+    document.getElementById('loading').style.display='block';
+    
     });  
+     
 }
 
+
 function getJson(){
-    
+    document.getElementById('loading').style.display="block";
     var id=document.querySelector('.active').id;
     var trending=`https://1boon.kakao.com/ch/trending.json?pagesize=10&page=${page}`;
     var issue=`https://1boon.kakao.com/ch/issue.json?pagesize=10&page=${page}`;
@@ -40,16 +46,21 @@ function getJson(){
                 var coverImage=data[i].coverImage;
                 var path=data[i].path;
                 var totalView=data[i].totalView;
-                str += `<span id='contain'> <span> <img src="${coverImage}"> </span> <div id="text"> <a href="https://1boon.kakao.com/${path}"> ${title} </a> </div> <div id="total"> 총 조회수: ${totalView} </div> </span>` ;
+                str += `<div id="contain"> <span> <img src="${coverImage}"> </span> <div id="text"> <a href="https://1boon.kakao.com/${path}"> ${title} </a> </div> <div id="total"> 총 조회수: ${totalView} </div> </div>` ;
             }
             list.innerHTML=str;
+            window.onload=closeLoding();
         })
     })
-    document.getElementById('more').style.display="block";
+    
 }
 
 //*************** 더보기 클릭. ***************//
 document.querySelector('.btn').addEventListener('click',function nextBtn(){
         page+=1;
         getJson()
+        document.getElementById('loading').style.display='block';
+        
+    ;
+        
 });
